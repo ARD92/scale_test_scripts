5f6f7e1f51e51daa81080162e17b87b26fb69338
aprabh92 <aprabh@juniper.net>
2022-11-18 08:50:33 -0500

    more args for handling

--- scale_firewall_filters/scale_test_ff.py
+++ scale_firewall_filters/scale_test_ff.py
@@ -6,6 +6,7 @@ import argparse
 import random
 from netaddr import *
 import time
+import os
 
 from jnpr.junos import Device
 from jnpr.junos.exception import ConnectError
@@ -15,8 +16,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-ip', action='store', dest='IP', type=str, help='ip address to connect to')
 parser.add_argument('-u', action='store', dest='USER', type=str, default='root', help='username of the device to connect to, default root')
 parser.add_argument('-p', action='store', dest='PASSWD', type=str, default='juniper123', help='password of device to connect to. default juniper123')
+parser.add_argument('-pt', action='store', dest='PORT', type=str, default=830, help='port to connect netonf. default is 830')
 parser.add_argument('-nf', action='store', dest='NUMF', type=int, default='1', help='Number of firewall filters')
+parser.add_argument('-nfr', action='store', dest='NUMRANGE', type=int, default=0, help='Filter start range')
 parser.add_argument('-nt', action='store',dest='NUMT',type=int,  help='Number of terms per filter')
+parser.add_argument('-ntr', action='store', dest='NUMTRANGE', type=int, default=0, help='Filter term start range')
 parser.add_argument('-sip', action='store', dest='SIP', type=str, help='source IP block to start. example. 1.0.0.0/8')
 parser.add_argument('-dip', action='store', dest='DIP',type=str, help='Dest IP block to start. example. 1.0.0.0/8')
 parser.add_argument('-edb', action='store', dest='EDB',type=str, default="false", help='set to true if need to program ephemeral db')
@@ -25,12 +29,12 @@ args=parser.parse_args()
 # List of protocols. THis will be chosen at random
 PROTOCOL = ['tcp', 'udp']
 
-def programScaledFilters(nf, nt, sip, dip):
+def programScaledFilters(nf, nfr, nt, ntr, sip, dip):
     # program filters at scale over netconf
     flist = []
     print("entering", nf, nt)
-    for numfilter in range(0,nf):
-        for numterms in range(0,nt):
+    for numfilter in range(nfr,nf):
+        for numterms in range(ntr,nt):
             sip = str(IPNetwork(sip).ip+numterms)
             dip = str(IPNetwork(dip).ip+numterms)
             proto = random.choice(PROTOCOL)
@@ -42,39 +46,51 @@ def programScaledFilters(nf, nt, sip, dip):
             cmd_dp = "set firewall family inet filter FF_{} term TERM_{} from destination-port {}".format(numfilter,numterms,dports)
             cmd_proto = "set firewall family inet filter FF_{} term TERM_{} from protocol {}".format(numfilter,numterms,proto)
             cmd_action = "set firewall family inet filter FF_{} term TERM_{} then accept".format(numfilter,numterms)
+            cmd_action_count = "set firewall family inet filter FF_{} term TERM_{} then count count-term-{}".format(numfilter,numterms, numterms)
             flist.append(cmd_sip)
             flist.append(cmd_dip)
             flist.append(cmd_sp)
             flist.append(cmd_dp)
             flist.append(cmd_proto)
             flist.append(cmd_action)
+            flist.append(cmd_action_count)
     return flist
 
 def main():
     # compile filter list
-    filterList = programScaledFilters(args.NUMF, args.NUMT, args.SIP, args.DIP)
+    filterList = programScaledFilters(args.NUMF, args.NUMRANGE, args.NUMT, args.NUMTRANGE, args.SIP, args.DIP)
     # Push to device
-    dev = Device(host=args.IP, user=args.USER, password=args.PASSWD)
+    dev = Device(host=args.IP, user=args.USER, password=args.PASSWD, port=int(args.PORT))
     try:
         dev.open()
         if args.EDB == "true":
             print("programming into ephemeral db with instance name EDB_FF_TEST")
             with Config(dev, mode='ephemeral', ephemeral_instance='EDB_FF_TEST') as cu:
-                print("loading config... ", time.time())
+                fp = open("/tmp/config.txt","w")
                 for data in filterList:
-                    cu.load(data,format='set')
+                    #cu.load(data,format='set')
+                    fp.write(data + "\n")
+                fp.close()
+                print("loading config... ", time.time())
+                cu.load(path="/tmp/config.txt", format='set')
                 print("commit config...",time.time())
                 cu.commit()
                 print("commit complete..",time.time())
+                os.remove("/tmp/config.txt")
 
         else:
             with Config(dev) as cu:
-                print("loading config... ", time.time())
+                fp = open("/tmp/config.txt","w")
                 for data in filterList:
-                    cu.load(data, format='set')
+                    #cu.load(data, format='set')
+                    fp.write(data + "\n");
+                fp.close()
+                print("loading config...", time.time())
+                cu.load(path="/tmp/config.txt", format='set', timeout=360)
                 print("commit config...",time.time())
-                cu.commit()
+                cu.commit(timeout=360)
                 print("commit complete..",time.time())
+                os.remove("/tmp/config.txt")
 
         dev.close()
     except Exception as e:
